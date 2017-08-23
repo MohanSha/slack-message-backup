d34d1c3d1759d636f7dbb07db913a85e71955f5c
Steven Garcia <steven.garcia@holbertonschool.com>
2017-08-23 13:46:29 -0700

    creating a file depending on the channel

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+import json
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -14,16 +15,41 @@ def info():
         data_distribution(data)
     return(":)")
 
-''' Checks if a new channel was created or if new messeges have arrived '''
-def data_distribution(data):
-    print(data)
 
-'''creates new file for channel '''
-def channel_created():
-    pass
+def data_distribution(data):
+    ''' Checks if a new channel was created or if new messeges have arrived '''
+
+    event_type = data["event"]["type"]
+
+    if event_type is "channel_created":
+        channel_created(data)
+    else if event_type is "message":
+        message_posted(data)
+    else:
+        print(json.dumps(data, indent=4))
+
+    print(json.dumps(data, indent=4))
+
+
+def channel_created(data):
+    '''creates new file for channel '''
+
+    channel_name = data["event"]["channel"]["name_normalized"]
+    channel_id = data["event"]["channel"]["id"]
+    file_path = "channels/{}.json".format(channel_name)
+    file = open(file_path, "w+")
+    file.close()
+
+    # add channel and id to a dict for reference
+    ref_file = open("channel_ref.json", "rw+")
+    ref = json.loads(ref_file.read())
+    ref.update({channel_id: channel_name})
+    ref_file.write(json.dumps(ref, indent=4))
+    ref_file.close()
 
 
 def message_posted():
+    print("message was posted")
     pass
 
 # Run the app :)
