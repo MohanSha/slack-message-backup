1d3261bf737efef6f5002223728d7ec3a7bba494
Steven Garcia <steven.garcia@holbertonschool.com>
2017-08-23 21:02:08 -0700

    test

--- app.py
+++ app.py
@@ -42,13 +42,12 @@ def channel_created(data):
 
     # add channel and id to a dict for reference
     ref_file = open("channel_ref.json", "w+")
-    try:
-        # Checking if the file is empty
-        ref = json.loads(ref_file.read())
-    except:
-        print("creating new dict")
-        return
-        ref = {}
+    # Checking if the file is empty
+    ref = json.loads(ref_file.read())
+    #except:
+    #    print("creating new dict")
+    #    return
+    #    ref = {}
     ref.update({channel_id: channel_name})
     ref_file.write(json.dumps(ref, indent=4))
     ref_file.close()
