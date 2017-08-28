6b5c304ddb7d3750b3cb48de854cef7e11add957
Steven Garcia <steven.garcia@holbertonschool.com>
2017-08-28 10:40:30 -0700

    adding the avility to create private channels in the channels folder

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def getting_channels(token):
         channel_ref.write(json.dumps(ref, indent=4))
         channel_ref.close()
 
-def getting_users():
+def getting_users(token):
     '''
         Creates references for users
     '''
@@ -68,9 +68,43 @@ def getting_users():
         user_file.close()
 
 
+def create_groups(token):
+    '''
+        Creates files for groups
+    '''
+
+    url = "https://slack.com/api/groups.list?token={}".format(token)
+    response = requests.get(url)
+    groups_list = response.json()
+
+    # Getting the channel_rerefence
+    with open ("channels/channel_ref.json", "a+")as channel_ref:
+        channel_ref.seek(0, 0)
+        channel_info = channel_ref.read()
+        channel_ref.close()
+
+    try:
+        ref = json.loads(channel_info)
+    except:
+        ref = {}
+
+    # Creating a file per private channel
+    for group in groups_list["groups"]:
+        if "mpdm" not in group["name"]:
+            file_path = "channels/{}.json".format(group["name"])
+        file = open(file_path, "w+")
+        file.close()
+        ref.update({group["id"]: group["name"]})
+
+    # Updating references for private channels
+    with open ("channels/channel_ref.json", "w")as channel_ref:
+        channel_ref.write(json.dumps(ref, indent=4))
+        channel_ref.close()
+
 
 
 if __name__ == "__main__":
     token = os.getenv("token_slack")
     getting_channels(token)
-    getting_users()
+    getting_users(token)
+    create_groups(token)
