3a275a2f7d6abf19f16fd97fef616665df4cdf3f
Steven Garcia <steven.garcia@holbertonschool.com>
2017-08-28 16:00:35 -0700

    backing up already existing history in channels

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 import os
 import requests
 import json
+from datetime import datetime
+from time import sleep
 
 '''
     Creates: Files for all the channels already created,
@@ -78,7 +80,7 @@ def create_groups(token):
     groups_list = response.json()
 
     # Getting the channel_rerefence
-    with open ("channels/channel_ref.json", "a+")as channel_ref:
+    with open ("groups/channel_ref.json", "a+")as channel_ref:
         channel_ref.seek(0, 0)
         channel_info = channel_ref.read()
         channel_ref.close()
@@ -91,7 +93,7 @@ def create_groups(token):
     # Creating a file per private channel
     for group in groups_list["groups"]:
         if "mpdm" not in group["name"]:
-            file_path = "channels/{}.json".format(group["name"])
+            file_path = "groups/{}.json".format(group["name"])
         file = open(file_path, "w+")
         file.close()
         ref.update({group["id"]: group["name"]})
@@ -101,10 +103,59 @@ def create_groups(token):
         channel_ref.write(json.dumps(ref, indent=4))
         channel_ref.close()
 
+def channel_history(token):
+
+    with open("channels/channel_ref.json", "r") as channel_info:
+        channel= json.loads(channel_info.read())
+        channel_info.close()
+
+    for id, name in channel.items():
+        sleep(10)
+        url = "https://slack.com/api/channels.history?token={}&channel={}".format(token,id)
+        response = requests.get(url)
+        history = response.json()
+        load = {}
+
+        for message in history["messages"]:
+            # Getting the user and message
+            try:
+                text = message["text"] # bandaid will fix later to handle files
+            except:
+                text = ""
+            try:
+                user = message["user"] # bandaid will fix later to handle files
+            except:
+                user = ""
+            ts = message["ts"]
+            time = datetime.fromtimestamp(float(ts)).strftime('%Y-%m-%d %H:%M:%S')
+            load.update({time : [{"user": user},{"text":text}]})
+
+        # Getting the file and writing to it
+        file = "channels/{}.json".format(name)
+
+        with open(file, "w") as history_file:
+            history_file.write(json.dumps(load, indent=4))
+            history_file.close()
+
+        # try:
+        #     file_history = json.loads(file_history)
+
+        # except:
+        #     file_history = {}
+
+        # file_history.update({})
+
+        # with open(file, "w") as history_file:
+        #     history_file(file_history)
+        #     history_file.close()
+
+
+
 
 
 if __name__ == "__main__":
     token = os.getenv("token_slack")
     getting_channels(token)
-    getting_users(token)
-    create_groups(token)
+    # getting_users(token)
+    # create_groups(token)
+    channel_history(token)
