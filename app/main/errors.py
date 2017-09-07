727725d7048b0699c8b74c8a404c4c4e73826a3d
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-06 18:33:04 -0700

    adding custom 404 page

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,7 +3,7 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return ("400")
+    return (render_template("404.html"))
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
